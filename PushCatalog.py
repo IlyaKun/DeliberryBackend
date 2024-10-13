3a2fac8d802fe2202335931aa9cf098fb64d4ece
Ilya <ilyathird14@gmail.com>
2024-10-13 11:46:35 +0300

    Updated JSON file 13.10.24 11:46:35

--- PushCatalog.py
+++ PushCatalog.py
@@ -1,11 +1,15 @@
 import csv
 import json
 import subprocess
+import os
+from datetime import datetime
 
 # Укажите путь к вашему CSV файлу
 csvFile = '/Users/ila/Documents/GitDeliberry/Catalog.csv'
 # Укажите путь для сохранения JSON файла
 jsonFile = '/Users/ila/Documents/GitDeliberry/CatalogJSON.json'
+
+
 
 # Чтение данных из CSV файла с использованием точки с запятой в качестве разделителя
 with open(csvFile, mode='r', encoding='utf-8') as file:
@@ -16,8 +20,13 @@ with open(csvFile, mode='r', encoding='utf-8') as file:
 with open(jsonFile, mode='w', encoding='utf-8') as file:
     json.dump(data, file, indent=4, ensure_ascii=False)  # Используем indent для форматирования
 
+# Получение текущей даты и времени
+current_time = datetime.now().strftime('%d.%m.%y %H:%M:%S')
+
+# Комментарий к коммиту с добавлением текущей даты и времени
+commit_message = f'Updated JSON file {current_time}'
 
 # Отправка изменений в Git
-#subprocess.run(['git', 'add', "."])
-#subprocess.run(['git', 'commit', '-m', 'Updated JSON file'])
-#subprocess.run(['git', 'push'])
+subprocess.run(['git', 'add', "."])
+subprocess.run(['git', 'commit', '-m', commit_message])
+subprocess.run(['git', 'push'])
